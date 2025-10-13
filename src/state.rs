e8e39bf458d8822e1f8cf922b47c512ee9b77128
amxrac <108365444+amxrac@users.noreply.github.com>
2025-10-13 20:24:13 +0100

    feat: add email registration service

--- src/state.rs
+++ src/state.rs
@@ -2,6 +2,7 @@ use crate::repositories::{
     EmailVerificationRepository, EmailVerificationRepositoryTrait, UserRepository,
     UserRepositoryTrait,
 };
+use crate::services::EmailService;
 use axum::extract::FromRef;
 use sqlx::PgPool;
 use std::sync::Arc;
@@ -11,6 +12,7 @@ pub struct AppState {
     pub db: PgPool,
     pub user_repository: Arc<dyn UserRepositoryTrait>,
     pub email_repository: Arc<dyn EmailVerificationRepositoryTrait>,
+    pub email_service: Arc<EmailService>,
 }
 
 impl AppState {
@@ -25,10 +27,14 @@ impl AppState {
         let email_repository: Arc<dyn EmailVerificationRepositoryTrait> =
             Arc::new(EmailVerificationRepository::new(db.clone()));
 
+        let email_service =
+            Arc::new(EmailService::new().expect("Failed to initialize email service"));
+
         Ok(Self {
             db,
             user_repository,
             email_repository,
+            email_service,
         })
     }
 }
