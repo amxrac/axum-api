7f839dc15f4b71947489ff7b9e443d7c7f7a72ee
amxrac <108365444+amxrac@users.noreply.github.com>
2025-10-19 11:58:18 +0100

    feat: add password reset token model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,7 @@
 pub mod email_verification_token;
+pub mod password_reset_tokens;
 pub mod user;
 
 pub use email_verification_token::EmailVerificationToken;
+pub use password_reset_tokens::PasswordResetToken;
 pub use user::User;
